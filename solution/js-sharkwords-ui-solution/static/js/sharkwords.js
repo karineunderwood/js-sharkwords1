daf68e82bce55cde0cdd024ecce32ba59d6d51ad
karineunderwood <karineunderwood23@gmail.com>
2022-04-05 13:05:45 -0700

    createDivs done

---
+++ solution/js-sharkwords-ui-solution/static/js/sharkwords.js
@@ -0,0 +1,67 @@
+const ALPHABET = 'abcdefghijklmnopqrstuvwxyz';
+
+const WORDS = [
+  'strawberry',
+  'orange',
+  'apple',
+  'banana',
+  'pineapple',
+  'kiwi',
+  'peach',
+  'pecan',
+  'eggplant',
+  'durian',
+  'peanut',
+  'chocolate',
+];
+
+const numWrong = 0;
+
+// Loop over the chars in `word` and create divs.
+const createDivsForChars = (word) => {
+  const wordContainer = document.querySelector('#word-container');
+  for (const letter of word) {
+    wordContainer.insertAdjacentHTML('beforeend', `<div class="letter-box ${letter}"></div>`);
+  }
+};
+
+// Loop over each letter in the alphabet and generate a button
+// for each letter
+const generateLetterButtons = () => {
+  const letterButtonContainer = document.querySelector('#letter-buttons');
+  for (const letter of ALPHABET) {
+    letterButtonContainer.insertAdjacentHTML('beforeend', `<button>${letter}</button>`);
+  }
+};
+
+// Disable the passed in button element
+//
+// `buttonEl` is an `HTMLElement` object.
+//
+const disableLetterButton = (buttonEl) => {
+  buttonEl.disabled = true;
+};
+
+// This is a helper function we will use in the future
+// It should return `true` if `letter` is in the word
+// For now, you should test it out to make sure it works
+
+const isLetterInWord = (letter) => document.querySelector(`div.${letter}`) !== null;
+
+// This is like if __name__ == '__main__' in Python
+// It will be called when the file is run (because
+// we call the function on line 66)
+(function startGame() {
+  // For now, we'll hardcode the word that the user has to guess
+  // You can change this to choose a random word from WORDS once you
+  // finish this lab but we hard code it so we know what the word is
+  // and can tell if things look correct for this word
+  const word = 'hello';
+
+  createDivsForChars(word);
+
+  generateLetterButtons();
+
+  // in the next lab, you will be adding functionality to handle when
+  // someone clicks on a letter
+})();
